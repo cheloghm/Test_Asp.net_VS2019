91a5961e7475f613e39d857665c364976ad2706a
Graham Elozonachukwu Chukwumaobi <cheloghm@gmail.com>
2021-01-12 23:43:00 +0000

    Added new controller and view

---
+++ Controllers/LinksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test_Asp.net_VS2019.Controllers
+{
+    public class LinksController : Controller
+    {
+        // GET: Links
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
